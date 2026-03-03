c09b3f8f957e437545f80147a07140f2d496a913
Dev Garg <gargdk2005@gmail.com>
2026-03-03 22:00:10 +0530

    storage.serve file added

---
+++ Backend_01/src/services/storage.service.js
@@ -0,0 +1,16 @@
+// require("dotenv").config();
+const {ImageKit}=require("@imagekit/nodejs")
+
+const imageKit=new ImageKit({
+    privateKey:process.env.IMAGEKIT_PRIVATE_KEY
+})
+
+async function uploadFile(buffer){
+    const result =await imageKit.files.upload({
+        file:buffer.toString("base64"),
+        fileName:"image.png"
+    })
+    return result
+}
+
+module.exports=uploadFile;
